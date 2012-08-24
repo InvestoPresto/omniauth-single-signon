42bf3d1c580086f5a982579c2339753ff255c7b4
sankaranarayanan <esankaranarayanan@gmail.com>
2012-08-24 14:03:13 +0530

    Fixed setting session user_return_to

--- app/controllers/omniauth_single_signon/sessions_controller.rb
+++ app/controllers/omniauth_single_signon/sessions_controller.rb
@@ -2,7 +2,7 @@ module OmniauthSingleSignon
   class SessionsController < ApplicationController
     def create
       session[:user_id] = User.find_or_create_by_omniauth(request.env["omniauth.auth"]).id
-      session[:user_return_to] = request.env['omniauth.origin']
+      session[:user_return_to] = request.env['omniauth.origin'] if request.env['omniauth.origin']
       redirect_to main_app.success_path, :notice => "Successfully signed in", :only_path => true
     end
 
