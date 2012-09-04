39483ed83f5392cb8410ec1780fc5d8983596f9e
code-R <vamsi@investopresto.com>
2012-09-04 15:59:21 +0530

    added params to distinguish sign_in and sign_up.

--- app/controllers/omniauth_single_signon/sessions_controller.rb
+++ app/controllers/omniauth_single_signon/sessions_controller.rb
@@ -1,9 +1,10 @@
 module OmniauthSingleSignon
   class SessionsController < ApplicationController
     def create
-      session[:user_id] = User.find_or_create_by_omniauth(request.env["omniauth.auth"]).id
+      user                     = User.find_or_create_by_omniauth(request.env["omniauth.auth"])
+      session[:user_id]        = user.id
       session[:user_return_to] = request.env['omniauth.origin'] if request.env['omniauth.origin']
-      redirect_to main_app.success_path, :notice => "Successfully signed in", :only_path => true
+      redirect_to main_app.success_path(:sign_up => user.sign_up), :notice => "Successfully signed in", :only_path => true
     end
 
     def destroy
